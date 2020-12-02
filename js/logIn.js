c9b4fabd5bd7cb0439b110240727b733cb7900f8
Hojiakbar-Turotov <73689981+Hojiakbar-Turotov@users.noreply.github.com>
2020-12-02 20:56:26 +0500

    effeckt qushdim

---
+++ js/logIn.js
@@ -0,0 +1,15 @@
+function openLogIn() {
+    const OPENLOGIN = document.getElementById("openLogIn");
+    const CLOSELOGIN = document.getElementById("closeLogIn");
+    const OPENNING = document.querySelectorAll("#openning");
+
+    const LOGIN = document.querySelectorAll(".login").value;
+    const PASSWORD = document.querySelectorAll(".password").value;
+    OPENLOGIN.classList.add("d-none");
+
+    if ((LOGIN === "hturotov") && (PASSWORD === 998933057927)) {
+        OPENNING.classList.remove("d-none")
+    } else {
+        CLOSELOGIN.classList.remove("d-none")
+    }
+}
