1c7094fb6711b7beffac64284657c50332ae8c65
Hojiakbar-Turotov <73689981+Hojiakbar-Turotov@users.noreply.github.com>
2020-12-06 12:26:53 +0500

    ura log in qismi tuliq ishladi

--- js/logIn.js
+++ js/logIn.js
@@ -1,19 +1,23 @@
 function openLogIn() {
-    setTimeout(function() {
-        const OPENLOGIN = document.getElementById("openLogIn");
-        const CLOSELOGIN = document.getElementById("closeLogIn");
-        const OPENNING = document.querySelectorAll("#openning");
+    // setTimeout(function() {
+    const OPENLOGIN = document.getElementById("openLogIn");
+    const CLOSELOGIN = document.getElementById("closeLogIn");
+    const OPENNING = document.querySelectorAll("#openning");
 
-        const LOGIN = document.querySelectorAll(".login").value;
-        const PASSWORD = document.querySelectorAll(".password").value;
+    const LOGIN = document.getElementById("loginid").value;
+    const PASSWORD = document.getElementById("passwordid").value;
+
+
+    if ((LOGIN === "hturotov") && (PASSWORD == 998933057927)) {
         OPENLOGIN.classList.add("d-none");
-
-        if ((LOGIN === "hturotov") && (PASSWORD === 998933057927)) {
-            OPENNING.classList.remove("d-none")
-        } else {
+        OPENNING.classList.remove("d-none")
+    } else {
+        setTimeout(function() {
+            OPENLOGIN.classList.add("d-none");
             CLOSELOGIN.classList.remove("d-none")
-        }
-    }, 3000)
+        }, 5000)
+    }
+    // }, 5000)
 
 
 }
