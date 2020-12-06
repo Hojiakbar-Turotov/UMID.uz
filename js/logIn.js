5e5320057737fe210ee531cac61a2be403cbb8a5
Hojiakbar-Turotov <73689981+Hojiakbar-Turotov@users.noreply.github.com>
2020-12-06 12:07:13 +0500

    ajoyib animatsiya qushdim

--- js/logIn.js
+++ js/logIn.js
@@ -1,20 +1,19 @@
 function openLogIn() {
-    const OPENLOGIN = document.getElementById("openLogIn");
-    const CLOSELOGIN = document.getElementById("closeLogIn");
-    const OPENNING = document.querySelectorAll("#openning");
+    setTimeout(function() {
+        const OPENLOGIN = document.getElementById("openLogIn");
+        const CLOSELOGIN = document.getElementById("closeLogIn");
+        const OPENNING = document.querySelectorAll("#openning");
 
-    const LOGIN = document.querySelectorAll(".login").value;
-    const PASSWORD = document.querySelectorAll(".password").value;
-    OPENLOGIN.classList.add("d-none");
+        const LOGIN = document.querySelectorAll(".login").value;
+        const PASSWORD = document.querySelectorAll(".password").value;
+        OPENLOGIN.classList.add("d-none");
 
-    if ((LOGIN === "hturotov") && (PASSWORD === 998933057927)) {
-        OPENNING.classList.remove("d-none")
-    } else {
-        CLOSELOGIN.classList.remove("d-none")
-    }
+        if ((LOGIN === "hturotov") && (PASSWORD === 998933057927)) {
+            OPENNING.classList.remove("d-none")
+        } else {
+            CLOSELOGIN.classList.remove("d-none")
+        }
+    }, 3000)
+
+
 }
-
-
-// function kutishRejimi (sekunt, minut, soat){
-//     const SOATQIYMATI= new("hou")
-// }
