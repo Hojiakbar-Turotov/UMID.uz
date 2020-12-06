2c54cf6331f7009c19fe70f1947d688000bdd553
Hojiakbar-Turotov <73689981+Hojiakbar-Turotov@users.noreply.github.com>
2020-12-06 16:52:46 +0500

    hello

--- js/logIn.js
+++ js/logIn.js
@@ -1,23 +1,20 @@
 function openLogIn() {
-    // setTimeout(function() {
-    const OPENLOGIN = document.getElementById("openLogIn");
-    const CLOSELOGIN = document.getElementById("closeLogIn");
-    const OPENNING = document.querySelectorAll("#openning");
+    setTimeout(function() {
+        const OPENLOGIN = document.getElementById("openLogIn");
+        const CLOSELOGIN = document.getElementById("closeLogIn");
+        const OPENNING = document.querySelectorAll("#openning");
 
-    const LOGIN = document.getElementById("loginid").value;
-    const PASSWORD = document.getElementById("passwordid").value;
+        const LOGIN = document.getElementById("loginid").value;
+        const PASSWORD = document.getElementById("passwordid").value;
 
 
-    if ((LOGIN === "hturotov") && (PASSWORD == 998933057927)) {
-        OPENLOGIN.classList.add("d-none");
-        OPENNING.classList.remove("d-none")
-    } else {
-        setTimeout(function() {
+        if ((LOGIN === "hturotov") && (PASSWORD == 998933057927)) {
+            OPENLOGIN.classList.add("d-none");
+            OPENNING.classList.remove("d-none")
+        } else {
+
             OPENLOGIN.classList.add("d-none");
             CLOSELOGIN.classList.remove("d-none")
-        }, 5000)
-    }
-    // }, 5000)
-
-
+        }
+    }, 5000)
 }
