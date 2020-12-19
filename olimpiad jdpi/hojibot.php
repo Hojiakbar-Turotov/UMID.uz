0661b64fcd620006d2e3f638a28fb06e1306e673
Hojiakbar-Turotov <73689981+Hojiakbar-Turotov@users.noreply.github.com>
2020-12-19 21:24:27 +0500

    sc

--- olimpiad jdpi/hojibot.php
+++ olimpiad jdpi/hojibot.php
@@ -1,7 +1,7 @@
 <? 
 date_default_timezone_set('Asia/Tashkent');
 define('API_KEY', '1455550727:AAFRUKsA_Lm8_uqCucnoyInomP5gQwpI-60');
-//https://api.telegram.org/bot1455550727:AAFRUKsA_Lm8_uqCucnoyInomP5gQwpI-60/setWebHook?url=https://soaat.uz/hojibot.php
+//https://api.telegram.org/bot1455550727:AAFRUKsA_Lm8_uqCucnoyInomP5gQwpI-60/setWebHook?url=https://github.com/Hojiakbar-Turotov/UMID.uz/blob/main/olimpiad%20jdpi/hojibot.php
 function bot($method, $datas = []){
     $url = "https://api.telegram.org/bot".API_KEY."/" . $method;
     $ch = curl_init();
