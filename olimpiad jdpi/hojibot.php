a52513939b31253aedc6956fc89368e47e9e99e2
Hojiakbar-Turotov <73689981+Hojiakbar-Turotov@users.noreply.github.com>
2020-12-30 13:24:58 +0500

    h

--- olimpiad jdpi/hojibot.php
+++ olimpiad jdpi/hojibot.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('Asia/Tashkent');
 define('API_KEY', '1455550727:AAFEbahKLAPUTbvz6sh0xlMLYT1K9awhle8');
-//https://api.telegram.org/1455550727:AAFEbahKLAPUTbvz6sh0xlMLYT1K9awhle8/setWebHook?url=https://github.com/Hojiakbar-Turotov/UMID.uz/blob/main/olimpiad%20jdpi/hojibot.php
+//https://api.telegram.org/bot1455550727:AAFEbahKLAPUTbvz6sh0xlMLYT1K9awhle8/setWebHook?url=https://umid-uz.netlify.app/olimpiad%20jdpi/hojibot.php
 function bot($method, $datas = []){
     $url = "https://api.telegram.org/bot".API_KEY."/" . $method;
     $ch = curl_init();
